5440d2a40619c8e9709499e41b22aed9bb2acf31
Yasvanth-2005 <yasvanthhanumantu1@gmail.com>
2024-03-12 10:58:12 +0530

    Setup Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
   );
 }
 
